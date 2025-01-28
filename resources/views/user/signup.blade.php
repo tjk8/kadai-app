b7e92e7d6812aa40d916ef63bc9dc21e0d3eb212
302A-14\cre <a>
2025-01-28 16:08:21 +0900

    a

--- resources/views/user/signup.blade.php
+++ resources/views/user/signup.blade.php
@@ -13,31 +13,71 @@
 
 <body class="">
     <x-header></x-header>
-    <div class="page singup-page">
-        <h2>課題: ここに新規登録ページを作ること</h2>
-        <h3>必要なフォームパーツ</h3>
-        <ul>
-            <li>Email入力フォーム</li>
-            <li>パスワード入力フォーム</li>
-            <li>登録ボタン</li>
-        </ul>
-        <h3>バリデーションルール</h3>
-        <h4>メールアドレス</h4>
-        <ul>
-            <li>●●●@×××の形式になっていること</li>
-            <li>半角英数、記号のみを許容すること</li>
-        </ul>
-        <h4>パスワード</h4>
-        <ul>
-            <li>半角英数、記号のみを許容すること</li>
-            <li>8文字以上であること</li>
-        </ul>
-        <p>
-            JavascriptとUserControllorの両方で同じバリデーションを設けること
-        </p>
+    <div class="page signup-page">
+        <form class="form" action="/signup" method="post">
+            <div class="form-item username">
+                <label for="username">username</label>
+                <input type="text" id="username" name="username" />
+            </div>
+            <div class="form-item email">
+                <label for="email">Email</label>
+                <input type="text" id="email" name="email" />
+            </div>
+            <div class="form-item password">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" />
+            </div>
+            <div class="login-button">
+                <button class="button-white" type="submit">新規作成</button>
+            </div>
+        </form>
     </div>
 </body>
-<script src="{{ asset('/js/app.js') }}"></script>
-<style scoped></style>
+
+<style scoped>
+    .signup-page {
+        display: flex;
+        justify-content: center;
+    }
+
+    .signup-page .title {
+        font-size: 24px;
+        font-weight: bold;
+        text-align: center;
+    }
+
+    .signup-page .form {
+        width: 80vw;
+    }
+
+    .signup-page input {
+        height: 30px;
+        border-radius: 10px;
+        background-color: lightgray;
+    }
+
+    .signup-page.login-page .form-item {
+        display: flex;
+        flex-direction: column;
+        margin-top: 10px;
+    }
+
+    .signup-page .login-button {
+        text-align: center;
+        margin-top: 30px;
+    }
+
+    .signup-page button {
+        width: 50%;
+        height: 30px;
+        font-size: 18px;
+    }
+
+    .signup-page .error-message {
+        margin-top: 5px;
+        font-size: 10px;
+    }
+</style>
+
 
 </html>
